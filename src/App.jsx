fcf34fd6776360898017d2f52188d08138ee8163
adrien <adrien.delestre@hotmail.com>
2022-12-13 13:27:37 +0100

    v1.0.3 - Ajustement du design

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,32 @@ import Form from './components/Form'
 import Results from './components/Results'
 
 function App() {
-  const useList = (initialList) => {
-    const [list, setList] = useState(initialList)
-    const add = (item) => {
-      setList([...list, item])
-    }
-    const remove = (item) => {
-      setList(list.filter((i) => i !== item))
-    }
-    return [list, add, remove]
-  }
-  const [symptoms, addSymptom, removeSymptom] = useList([])
-  return (
-    <div className="relative flex flex-col gap-6 md:w-3/4 max-w-5xl mx-6 md:mx-auto py-6 items-center">
-      <div className="pb-8 w-full capitalize text-3xl border-b-2">
-        système expert - aide au diagnostic médical - infirmerie
-      </div>
-      <div className="flex flex-col gap-2 relative w-full">
-        <Form
-          symptoms={symptoms}
-          addSymptom={addSymptom}
-          removeSymptom={removeSymptom}
-        />
-        <Results symptoms={symptoms} />
-      </div>
-    </div>
-  )
+	const useList = (initialList) => {
+		const [list, setList] = useState(initialList)
+		const add = (item) => {
+			setList([...list, item])
+		}
+		const remove = (item) => {
+			setList(list.filter((i) => i !== item))
+		}
+		return [list, add, remove]
+	}
+	const [symptoms, addSymptom, removeSymptom] = useList([])
+	return (
+		<div className="relative flex flex-col gap-6 md:w-3/4 max-w-5xl mx-6 md:mx-auto py-6 items-center">
+			<div className="relative pb-8 w-full capitalize text-xl sm:text-2xl md:text-3xl border-b-2">
+				système expert - aide au diagnostic médical - infirmerie
+			</div>
+			<div className="flex flex-col gap-2 relative w-full">
+				<Form
+					symptoms={symptoms}
+					addSymptom={addSymptom}
+					removeSymptom={removeSymptom}
+				/>
+				<Results symptoms={symptoms} />
+			</div>
+		</div>
+	)
 }
 
 export default App
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,57 +3,55 @@ import db from '../res/new.json'
 import { getFact } from '../res/dbAPI'
 
 function Results({ symptoms }) {
-  const updatedFacts = []
-  symptoms.forEach((symptom) => updatedFacts.push(symptom.id))
-  const getResults = () => {
-    let factAdded = false
-    db.regles.forEach((regle) => {
-      if (
-        !updatedFacts.includes(regle.consequence) &&
-        regle.hypotesis.every((symptom) => {
-          return updatedFacts.includes(symptom)
-        })
-      ) {
-        updatedFacts.push(regle.consequence)
-        factAdded = true
-      }
-    })
-    if (factAdded) return getResults()
-    else return filterResults(updatedFacts)
-  }
-  const filterResults = (facts) => {
-    let ResultsToRemove = []
-    facts.forEach((factId) => {
-      db.regles.forEach((regle) => {
-        if (regle.consequence === factId) {
-          regle.hypotesis.forEach((symptom) => {
-            if (facts.includes(symptom) && !ResultsToRemove.includes(factId)) {
-              ResultsToRemove.push(symptom)
-            }
-          })
-        }
-      })
-    })
-    return facts.filter((fact) => !ResultsToRemove.includes(fact))
-  }
-  return (
-    <div className="flex flex-col gap-6 items-center bg-slate-100 py-2 px-6 border-2 rounded-md border-gray-300">
-      <div className="text-lg">Diagnostic :</div>
-      <div>
-        <div className="flex flex-row gap-2 items-center">
-          <div className="text-2xl">
-            {getResults() &&
-              getResults().map((result) => {
-                let fact = getFact(result)
-                return fact.categorie !== 'primaire' ? (
-                  <div key={fact.id}>{fact.name}</div>
-                ) : null
-              })}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+	const updatedFacts = []
+	symptoms.forEach((symptom) => updatedFacts.push(symptom.id))
+	const getResults = () => {
+		let factAdded = false
+		db.regles.forEach((regle) => {
+			if (
+				!updatedFacts.includes(regle.consequence) &&
+				regle.hypotesis.every((symptom) => {
+					return updatedFacts.includes(symptom)
+				})
+			) {
+				updatedFacts.push(regle.consequence)
+				factAdded = true
+			}
+		})
+		if (factAdded) return getResults()
+		else return filterResults(updatedFacts)
+	}
+	const filterResults = (facts) => {
+		let ResultsToRemove = []
+		facts.forEach((factId) => {
+			db.regles.forEach((regle) => {
+				if (regle.consequence === factId) {
+					regle.hypotesis.forEach((symptom) => {
+						if (facts.includes(symptom) && !ResultsToRemove.includes(factId)) {
+							ResultsToRemove.push(symptom)
+						}
+					})
+				}
+			})
+		})
+		return facts.filter((fact) => !ResultsToRemove.includes(fact))
+	}
+	return (
+		<div className="flex flex-col gap-2 bg-slate-100 py-2 px-6 border-2 rounded-md border-gray-300">
+			<div className="text-lg">Diagnostic :</div>
+			<div>
+				<ul className="flex flex-col gap-2 px-6 text-xl list-disc">
+					{getResults() &&
+						getResults().map((result) => {
+							let fact = getFact(result)
+							return fact.categorie !== 'primaire' ? (
+								<li key={fact.id}>{fact.name}</li>
+							) : null
+						})}
+				</ul>
+			</div>
+		</div>
+	)
 }
 
 export default Results
