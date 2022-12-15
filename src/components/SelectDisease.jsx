8c9c0d0724967493bb63fa109f05865c82b6add6
adrien <adrien.delestre@hotmail.com>
2022-12-15 22:56:55 +0100

    v1.0.3 - Ajout du protocol et des filtres

--- src/components/SelectDisease.jsx
+++ src/components/SelectDisease.jsx
@@ -2,31 +2,31 @@ import React from 'react'
 import { getSymptomFromCategory } from '../res/dbAPI'
 
 function SelectDisease({ selectedDisease, setSelectedDisease }) {
-  const disease = getSymptomFromCategory('intermediaire').concat(
-    getSymptomFromCategory('terminal')
-  )
-  return (
-    <div>
-      <div className="text-lg">Sélectionnez une maladie :</div>
-      <form className="grid grid-cols-3 justify-center">
-        {disease.map((disease, index) => (
-          <div key={index}>
-            <label className="form-check-label inline-block text-gray-800">
-              <input
-                className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                type="radio"
-                name={disease.name}
-                checked={selectedDisease === disease.id}
-                onClick={() => setSelectedDisease(disease.id)}
-                onChange={() => {}}
-              />
-              {disease.name}
-            </label>
-          </div>
-        ))}
-      </form>
-    </div>
-  )
+	const disease = getSymptomFromCategory('intermediaire').concat(
+		getSymptomFromCategory('terminal')
+	)
+	return (
+		<div>
+			<div className="text-lg">Sélectionnez une maladie :</div>
+			<form className="grid grid-cols-3 justify-center">
+				{disease.map((disease, index) => (
+					<div key={index}>
+						<label className="form-check-label inline-block text-gray-800">
+							<input
+								className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+								type="radio"
+								name={disease.name}
+								checked={selectedDisease === disease.id}
+								onClick={() => setSelectedDisease(disease.id)}
+								onChange={() => {}}
+							/>
+							{disease.name}
+						</label>
+					</div>
+				))}
+			</form>
+		</div>
+	)
 }
 
 export default SelectDisease
--- src/res/dbAPI.js
+++ src/res/dbAPI.js
@@ -1,43 +1,41 @@
-import db from './new.json'
+import db from './db.json'
 
 const getFact = (id) => {
-  return db.faits.find((d) => d.id === id)
+	return db.faits.find((d) => d.id === id)
 }
 
 const getCategories = () => {
-  let categories = []
-  db.faits.forEach((fait) => {
-    if (fait.categories) {
-      fait.categories.forEach((category) => {
-        if (!categories.includes(category)) {
-          categories.push(category)
-        }
-      })
-    }
-  })
-  return categories
+	let categories = []
+	db.faits.forEach((fait) => {
+		if (fait.nature) {
+			if (!categories.includes(fait.nature)) {
+				categories.push(fait.nature)
+			}
+		}
+	})
+	return categories
 }
 
 const getRuleFromConsquence = (consequence) => {
-  let rule = null
-  db.regles.forEach((regle) => {
-    if (regle.consequence === consequence) {
-      rule = regle
-    }
-  })
-  return rule
+	let rule = null
+	db.regles.forEach((regle) => {
+		if (regle.consequence === consequence) {
+			rule = regle
+		}
+	})
+	return rule
 }
 
 const getSymptomFromCategory = (category) => {
-  let symptoms = []
-  db.faits.forEach((fait) => {
-    if (fait.categorie) {
-      if (fait.categorie.includes(category)) {
-        symptoms.push(fait)
-      }
-    }
-  })
-  return symptoms
+	let symptoms = []
+	db.faits.forEach((fait) => {
+		if (fait.categorie) {
+			if (fait.categorie.includes(category)) {
+				symptoms.push(fait)
+			}
+		}
+	})
+	return symptoms
 }
 
 export { getFact, getCategories, getSymptomFromCategory, getRuleFromConsquence }
