72f9c107669f2af12b361d395899b4083163acff
adelestre <adrien.delestre@otmail.com>
2022-12-16 11:47:58 +0100

    v1.0.13 Added Trace

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,6 +5,7 @@ import { getFact } from '../res/dbAPI'
 function Results({ symptoms }) {
   const updatedFacts = []
   symptoms.forEach((symptom) => updatedFacts.push(symptom.id))
+  console.log('Tableau des symptômes : ', updatedFacts)
   const getResults = () => {
     let factAdded = false
     db.regles.forEach((regle) => {
@@ -15,11 +16,16 @@ function Results({ symptoms }) {
         })
       ) {
         updatedFacts.push(regle.consequence)
+        console.log('Ajout de ', regle.consequence, ' dans le diagnostic')
         factAdded = true
       }
     })
-    if (factAdded) return getResults()
-    else return filterResults(updatedFacts)
+    if (factAdded) {
+      console.log(
+        'Un fait ou plusieurs faits ont été déterminés, on relance la fonction de recherche'
+      )
+      return getResults()
+    } else return filterResults(updatedFacts)
   }
   const filterResults = (facts) => {
     let ResultsToRemove = []
@@ -34,9 +40,16 @@ function Results({ symptoms }) {
         }
       })
     })
+    if (ResultsToRemove.length > 0)
+      console.log(
+        'Tableau des maladies secondaires à ne pas afficher dans le diagnostic : ',
+        ResultsToRemove
+      )
     return facts.filter((fact) => !ResultsToRemove.includes(fact))
   }
   let results = getResults()
+    .map((result) => getFact(result))
+    .filter((fact) => fact.categorie !== 'primaire')
   return (
     <div className="flex flex-col gap-2 bg-slate-100 py-2 px-6 border-2 rounded-md border-gray-300">
       <div className="text-lg">Diagnostic :</div>
@@ -44,31 +57,30 @@ function Results({ symptoms }) {
         <ul className="flex flex-col gap-4 px-6 text-xl list-disc">
           {results &&
             results.length > 0 &&
-            results.map((result) => {
-              let fact = getFact(result)
-              return fact.categorie !== 'primaire' ? (
-                <div>
+            results.map((fact) => {
+              return (
+                <div key={fact.id}>
                   <li
-                    key={fact.id}
-                    className="font-semibold"
+                    className="font-semibold capitalize"
                     style={{ color: 'green' }}
                   >
                     {fact.name}
                   </li>
                   <ul>
                     <li className="ml-6">Protocole : </li>
-                    {fact.protocol.map((protocol, index) => {
-                      return (
-                        <li key={index} className="ml-6">
-                          {'=> ' + (index + 1) + '. ' + protocol}
-                        </li>
-                      )
-                    })}
+                    {fact.protocol &&
+                      fact.protocol.map((protocol, index) => {
+                        return (
+                          <li key={index} className="ml-6">
+                            {'=> ' + (index + 1) + '. ' + protocol}
+                          </li>
+                        )
+                      })}
                   </ul>
                 </div>
-              ) : null
+              )
             })}
-          {results && results.length === 0 && (
+          {results.length === 0 && (
             <div className="text-center font-semibold" style={{ color: 'red' }}>
               Aucune maladie trouvée
             </div>
